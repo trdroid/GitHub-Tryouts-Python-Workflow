702aa62486b44519bdb323bc530ee49998b05bb6
Jai <gruprog@gmail.com>
2025-08-26 13:32:30 -0400

    Updated the script to use the installed Python packages

--- script.py
+++ script.py
@@ -1,4 +1,6 @@
 import json
+import requests
+import rich_argparse
 
 user_data = {
     "id": 1234,
@@ -7,5 +9,8 @@ user_data = {
 }
 
 json_output = json.dumps(user_data, indent=4)
+
+print(f"requests: {requests}")
+print(f"rich_argparse: {rich_argparse}")
 
 print(json_output)
